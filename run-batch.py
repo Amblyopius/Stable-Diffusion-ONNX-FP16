5b8d84d7cb186e0dad221c6a0ed8b813ec523c79
Amblyopius <Amblyopius@users.noreply.github.com>
2023-02-03 13:16:14 +0000

    Update run-batch.py
    - Support loading Text Encoder on CPU
    - Support generating multiple resolutions

--- run-batch.py
+++ run-batch.py
@@ -21,6 +21,8 @@ import json
 import gc
 # We want to replace the text encoder in the pipeline
 import functools
+# We want to parse arguments
+import argparse
 # Numpy is used to provide a random generator
 import numpy
 # We need to load images for img2img
@@ -28,8 +30,9 @@ import numpy
 from PIL import Image, PngImagePlugin
 
 # The pipelines
-from diffusers import OnnxStableDiffusionPipeline
-from diffusers import OnnxStableDiffusionImg2ImgPipeline
+from diffusers import OnnxStableDiffusionPipeline, OnnxStableDiffusionImg2ImgPipeline
+# Model needed to load Text Encoder on CPU
+from diffusers import OnnxRuntimeModel
 # The schedulers
 from diffusers import (
     DDIMScheduler,
@@ -51,6 +54,7 @@ import lpw_pipe
 defSettings = {
     "width": 512,
     "height": 512,
+    "reslist": [],
     "steps": 30,
     "stepslist": [],
     "scale": 7.5,
@@ -70,8 +74,24 @@ defSettings = {
     "strengthlist": []
 }
 
-args = sys.argv
-for proj in args[1:] :
+parser = argparse.ArgumentParser()
+
+parser.add_argument(
+    "--cpu-textenc",
+    action="store_true",
+    help="Load Text Encoder on CPU to save VRAM"
+)
+
+parser.add_argument(
+    'project',
+    nargs='+',
+    type=str,
+    help="Provide projects as directories that contain settings.json"
+)
+
+args = parser.parse_args()
+
+for proj in args.project:
     print("Running project "+proj)
     # Check for directory
     if os.path.isdir(proj):
@@ -104,23 +124,47 @@ for proj in args[1:] :
                 }
                 if runSettings['task']=="img2img":
                     init_image = Image.open(infile).convert("RGB")
-                    pipe = OnnxStableDiffusionImg2ImgPipeline.from_pretrained(
-                        model,
-                        provider="DmlExecutionProvider",
-                        revision="onnx",
-                        scheduler=sched['pndm'],
-                        safety_checker=None,
-                        feature_extractor=None
-                    )
+                    if args.cpu_textenc:
+                        cputextenc=OnnxRuntimeModel.from_pretrained(model+"/text_encoder")
+                        pipe = OnnxStableDiffusionImg2ImgPipeline.from_pretrained(
+                            model,
+                            provider="DmlExecutionProvider",
+                            revision="onnx",
+                            scheduler=sched['pndm'],
+                            text_encoder=cputextenc,
+                            safety_checker=None,
+                            feature_extractor=None
+                        )
+                    else:
+                        pipe = OnnxStableDiffusionImg2ImgPipeline.from_pretrained(
+                            model,
+                            provider="DmlExecutionProvider",
+                            revision="onnx",
+                            scheduler=sched['pndm'],
+                            safety_checker=None,
+                            feature_extractor=None
+                        )
                 else:
-                    pipe = OnnxStableDiffusionPipeline.from_pretrained(
-                        model,
-                        provider="DmlExecutionProvider",
-                        revision="onnx",
-                        scheduler=sched['pndm'],
-                        safety_checker=None,
-                        feature_extractor=None
-                    )
+                    if args.cpu_textenc:
+                        cputextenc=OnnxRuntimeModel.from_pretrained(model+"/text_encoder")
+                        pipe = OnnxStableDiffusionPipeline.from_pretrained(
+                            model,
+                            provider="DmlExecutionProvider",
+                            revision="onnx",
+                            scheduler=sched['pndm'],
+                            text_encoder=cputextenc,
+                            safety_checker=None,
+                            feature_extractor=None
+                        )
+                    else:
+                        pipe = OnnxStableDiffusionPipeline.from_pretrained(
+                            model,
+                            provider="DmlExecutionProvider",
+                            revision="onnx",
+                            scheduler=sched['pndm'],
+                            safety_checker=None,
+                            feature_extractor=None
+                        )                       
                 if runSettings['textenc'] == "lpw":
                     pipe._encode_prompt = functools.partial(lpw_pipe._encode_prompt, pipe)
                 generator = numpy.random
@@ -136,6 +180,13 @@ for proj in args[1:] :
                     seedlist=range(runSettings['seed'],runSettings['seedend']+1)
                 else:
                     seedlist=runSettings['seedlist']
+                # Set resolustions for project
+                if len(runSettings['reslist'])==0:
+                    restuples=[(runSettings['width'],runSettings['height'])]
+                else:
+                    restuples=[]
+                    for resstr in runSettings['reslist']:
+                        restuples.append(tuple(map(int, resstr.split("x"))))
                 # Set steps for project
                 if len(runSettings['stepslist'])==0:
                     stepslist=[runSettings['steps']]
@@ -156,7 +207,7 @@ for proj in args[1:] :
                     strengthlist=[runSettings['strength']]
                 else:
                     strengthlist=runSettings['strengthlist']
-                imgnr=len(schedulerlist)*len(promptlist)*len(seedlist)*len(stepslist)*len(scalelist)*len(strengthlist)
+                imgnr=len(schedulerlist)*len(promptlist)*len(seedlist)*len(restuples)*len(stepslist)*len(scalelist)*len(strengthlist)
                 imgdone=0
                 for scheduler in schedulerlist:
                     if not sched[scheduler]:
@@ -165,53 +216,54 @@ for proj in args[1:] :
                     promptnum=0
                     for prompt in promptlist:
                         for seed in seedlist:
-                            for steps in stepslist:
-                                for scale in scalelist:
-                                    for strength in strengthlist:
-                                        if runSettings['task']=="img2img":
-                                            filename=(
-                                                f"{proj}/result-p{promptnum}-seed{seed}-steps-{steps}-"+
-                                                f"{scheduler}-scale-"+str(scale).replace(".","_")+
-                                                "-strength-"+str(strength).replace(".","_")+".png"
-                                            )
-                                        else:
-                                            filename=(
-                                                f"{proj}/result-p{promptnum}-seed{seed}-steps-{steps}-"+
-                                                f"{scheduler}-scale-"+str(scale).replace(".","_")+".png"
-                                            )
-                                        if not os.path.isfile(filename):
-                                            generator.seed(seed)
+                            for res in restuples:
+                                for steps in stepslist:
+                                    for scale in scalelist:
+                                        for strength in strengthlist:
                                             if runSettings['task']=="img2img":
-                                                image = pipe(
-                                                    image=init_image,
-                                                    strength=strength,
-                                                    prompt=prompt,
-                                                    negative_prompt=runSettings['negative_prompt'],
-                                                    num_inference_steps=steps,
-                                                    guidance_scale=scale,
-                                                    generator=generator).images[0]
+                                                filename=(
+                                                    f"{proj}/result-p{promptnum}-seed{seed}-{res[0]}x{res[1]}-"+
+                                                    f"-steps-{steps}-{scheduler}-scale-"+str(scale).replace(".","_")+
+                                                    "-strength-"+str(strength).replace(".","_")+".png"
+                                                )
                                             else:
-                                                image = pipe(
-                                                    prompt=prompt,
-                                                    negative_prompt=runSettings['negative_prompt'],
-                                                    height=runSettings['height'],
-                                                    width=runSettings['width'],
-                                                    num_inference_steps=steps,
-                                                    guidance_scale=scale,
-                                                    generator = generator).images[0]
-                                            metadata = PngImagePlugin.PngInfo()
-                                            metadata.add_text("Generator","Stable Diffusion ONNX https://github.com/Amblyopius/Stable-Diffusion-ONNX-FP16")
-                                            metadata.add_text("SD Model (local name)",model)
-                                            metadata.add_text("SD Prompt",prompt)
-                                            metadata.add_text("SD Negative Prompt",runSettings['negative_prompt'])
-                                            metadata.add_text("SD Scheduler",scheduler)
-                                            metadata.add_text("SD Steps",str(steps))
-                                            metadata.add_text("SD Guidance Scale",str(scale))
-                                            image.save(filename, pnginfo = metadata)
-                                        else:
-                                            print("Skipping existing image!")
-                                        imgdone+=1
-                                        print(f"Finished {imgdone}/{imgnr}")
+                                                filename=(
+                                                    f"{proj}/result-p{promptnum}-seed{seed}-{res[0]}x{res[1]}-"+
+                                                    f"-steps-{steps}-{scheduler}-scale-"+str(scale).replace(".","_")+".png"
+                                                )
+                                            if not os.path.isfile(filename):
+                                                generator.seed(seed)
+                                                if runSettings['task']=="img2img":
+                                                    image = pipe(
+                                                        image=init_image,
+                                                        strength=strength,
+                                                        prompt=prompt,
+                                                        negative_prompt=runSettings['negative_prompt'],
+                                                        num_inference_steps=steps,
+                                                        guidance_scale=scale,
+                                                        generator=generator).images[0]
+                                                else:
+                                                    image = pipe(
+                                                        prompt=prompt,
+                                                        negative_prompt=runSettings['negative_prompt'],
+                                                        width=res[0],
+                                                        height=res[1],
+                                                        num_inference_steps=steps,
+                                                        guidance_scale=scale,
+                                                        generator = generator).images[0]
+                                                metadata = PngImagePlugin.PngInfo()
+                                                metadata.add_text("Generator","Stable Diffusion ONNX https://github.com/Amblyopius/Stable-Diffusion-ONNX-FP16")
+                                                metadata.add_text("SD Model (local name)",model)
+                                                metadata.add_text("SD Prompt",prompt)
+                                                metadata.add_text("SD Negative Prompt",runSettings['negative_prompt'])
+                                                metadata.add_text("SD Scheduler",scheduler)
+                                                metadata.add_text("SD Steps",str(steps))
+                                                metadata.add_text("SD Guidance Scale",str(scale))
+                                                image.save(filename, pnginfo = metadata)
+                                            else:
+                                                print("Skipping existing image!")
+                                            imgdone+=1
+                                            print(f"Finished {imgdone}/{imgnr}")
                         promptnum+=1
                 del pipe
                 gc.collect()
